a8f1214faddef7b24c736eaeebfe29ffb2e5a96a
Alisha <asmaahsanam@gmail.com>
2026-09-14 17:44:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function show()
+{
+    var date = new Date();
+
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+    var d = date.getDate();
+    var mo = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    var session = "AM";
+
+    if (h >= 12)
+    {
+        session = "PM";
+    }
+
+    if (h > 12)
+    {
+        h = h - 12;
+    }
+
+    if (h == 0)
+    {
+        h = 12;
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+
+    document.getElementById("Clock").innerText = time;
+
+    var todayDate = d + "/" + mo + "/" + year;
+
+    document.getElementById("Date").innerText = todayDate;
+
+    setTimeout(show, 1000);
+}
+
+show();
